39bd8f34a8f99e9bbbbb293823c5bc57de63ad65
HSaqui <hsaqui@unomaha.edu>
2024-08-28 16:43:08 +0000

    first update

--- FirstProgram.py
+++ FirstProgram.py
@@ -4,19 +4,20 @@
 #Assignment:
 
 def main():
-  print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
-
+  name = input("what is your name")
   #Use the user's name in the program.
-
+  print("Good to meet you",name)
   #Ask the user for their age.
-
+  age = input("how old are you")
   #Tell the user what year they were born in.
+  
+  age = int(age)
+  Birthyear = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
-
+  print("2024", Birthyear )
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -6,10 +6,11 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+  noun1 = input("enter a noun")
+  name1 = "Todd"
   #Print the story with the user supplied words.
 
-
+  print("i like to ride a" + noun1 + "with mr friend " + name1)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
